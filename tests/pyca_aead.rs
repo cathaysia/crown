3acf4bfec048507933a14f1131b6e57a2136a800
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-07 18:57:38 +0800

    test: fix test
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- tests/pyca_aead.rs
+++ tests/pyca_aead.rs
@@ -1,14 +1,23 @@
-use kittycrypto::envelope::EvpAeadCipher;
+mod utils;
+use kittycrypto::{envelope::EvpAeadCipher, error::CryptoResult};
+
+use crate::utils::parse_response_line;
 
 #[test]
-fn test_pyca_ocb3_vectors() {
+fn test_pyca_aead_vectors() {
     const BASE_DIR: &str = "tests/cryptography/vectors/cryptography_vectors/";
-    const FILES: [&str; 2] = [
-        "ciphers/AES/OCB3/rfc7253.txt",
-        "ciphers/AES/OCB3/test-vector-1-nonce104.txt",
+    const FILES: [(fn(key: &[u8]) -> CryptoResult<EvpAeadCipher>, &str); 2] = [
+        (
+            EvpAeadCipher::new_aes_ocb3::<16, 12>,
+            "ciphers/AES/OCB3/rfc7253.txt",
+        ),
+        (
+            EvpAeadCipher::new_aes_ocb3::<16, 13>,
+            "ciphers/AES/OCB3/test-vector-1-nonce104.txt",
+        ),
     ];
 
-    for filename in FILES {
+    for (newer, filename) in FILES {
         let content = std::fs::read_to_string(format!("{BASE_DIR}/{filename}")).unwrap();
         let mut lines = content.lines();
 
@@ -34,27 +43,30 @@ fn test_pyca_ocb3_vectors() {
                         continue;
                     }
 
-                    if let Some(key_hex) = line.strip_prefix("Key = ") {
-                        key = Some(hex::decode(key_hex).unwrap());
-                    } else if let Some(nonce_hex) = line.strip_prefix("Nonce = ") {
-                        nonce = Some(hex::decode(nonce_hex).unwrap());
-                    } else if let Some(aad_hex) = line.strip_prefix("AAD = ") {
-                        if aad_hex.is_empty() {
-                            aad = Some(Vec::new());
-                        } else {
-                            aad = Some(hex::decode(aad_hex).unwrap());
+                    let Ok((rkey, value)) = parse_response_line(line) else {
+                        eprintln!("parse response line failed: {line}");
+                        continue;
+                    };
+
+                    match rkey.as_str() {
+                        "key" => {
+                            key = Some(value);
                         }
-                    } else if let Some(pt_hex) = line.strip_prefix("Plaintext = ") {
-                        if pt_hex.len() % 2 != 0 {
-                            continue;
+                        "plaintext" => {
+                            plaintext = Some(value);
                         }
-                        if pt_hex.is_empty() {
-                            plaintext = Some(Vec::new());
-                        } else {
-                            plaintext = Some(hex::decode(pt_hex).unwrap());
+                        "ciphertext" => {
+                            expected_ciphertext = Some(value);
                         }
-                    } else if let Some(ct_hex) = line.strip_prefix("Ciphertext = ") {
-                        expected_ciphertext = Some(hex::decode(ct_hex).unwrap());
+                        "nonce" => {
+                            nonce = Some(value);
+                        }
+                        "aad" => {
+                            aad = Some(value);
+                        }
+                        _ => {
+                            eprintln!("unexpected key: {rkey}");
+                        }
                     }
                 }
 
@@ -66,7 +78,7 @@ fn test_pyca_ocb3_vectors() {
                     Some(expected_ciphertext),
                 ) = (key, nonce, aad, plaintext, expected_ciphertext)
                 {
-                    let cipher = EvpAeadCipher::new_aes_ocb3::<16, 12>(&key).unwrap();
+                    let cipher = newer(&key).unwrap();
 
                     let tag = cipher
                         .seal_in_place_separate_tag(&mut plaintext, &nonce, &aad)
