05a3bf9ebe6d7eadc3b85b4ccd1283b8415062dc
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-14 21:43:05 +0800

    test(bench): rewrite des benchmark, remove twofish benchmark
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crown/benches/des.rs
+++ crown/benches/des.rs
@@ -1,47 +1,88 @@
-use cipher::KeyInit;
-use cipher::{generic_array::GenericArray, BlockEncrypt};
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use crown::block::des::Des;
-use crown::block::BlockCipher;
-use std::hint::black_box;
 
 fn bench_des(c: &mut Criterion) {
-    let mut key = [0u8; 8];
+    let mut key = [0u8; 32];
     rand::fill(&mut key);
     let key = key;
 
     let mut block = [0u8; 4];
     rand::fill(&mut block);
 
-    let mut group = c.benchmark_group("des");
-    group.throughput(Throughput::Bytes(4));
+    let mut iv = [0u8; 16];
+    rand::fill(&mut iv);
 
-    group.bench_function("crown_des".to_string(), |b| {
-        let cipher = crown::block::des::Des::new(&key).unwrap();
-        b.iter(|| {
-            let mut dst = block;
-            for i in (0..block.len()).step_by(Des::BLOCK_SIZE) {
-                let end = (i + Des::BLOCK_SIZE).min(block.len());
-                if end - i == Des::BLOCK_SIZE {
-                    cipher.encrypt_block(black_box(&mut dst[i..end]));
-                }
-            }
-        })
-    });
+    let mut nonce = [0u8; 12];
+    rand::fill(&mut nonce);
 
-    group.bench_function("rustcrypto_des".to_string(), |b| {
-        let cipher = des::Des::new(&key.into());
+    let cases = [128, 512, 1024];
 
-        b.iter(|| {
-            let mut dst = block;
-            for chunk in dst.chunks_exact_mut(Des::BLOCK_SIZE) {
-                let block = GenericArray::from_mut_slice(chunk);
-                cipher.encrypt_block(black_box(block));
-            }
-        })
-    });
+    for i in cases {
+        let mut block = vec![0u8; i];
+        rand::fill(block.as_mut_slice());
 
-    group.finish();
+        let mut group = c.benchmark_group(format!("des_cbc_{i}"));
+        group.throughput(Throughput::Bytes(i as u64));
+
+        group.bench_function("crown".to_string(), |b| {
+            let mut cipher = crown::envelope::EvpBlockCipher::new_des_cbc(&key, &iv).unwrap();
+            let mut block = block.to_vec();
+            b.iter(|| {
+                let _ = cipher.encrypt_alloc(&mut block);
+            })
+        });
+
+        group.bench_function("boring".to_string(), |b| {
+            let cipher = boring::symm::Cipher::des_cbc();
+
+            b.iter(|| {
+                let _ = boring::symm::encrypt(cipher, &key, Some(&iv), &block);
+            });
+        });
+        group.finish();
+    }
+
+    for i in cases {
+        let mut block = vec![0u8; i];
+        rand::fill(block.as_mut_slice());
+
+        let mut group = c.benchmark_group(format!("des_ctr_{i}"));
+        group.throughput(Throughput::Bytes(i as u64));
+
+        group.bench_function("crown".to_string(), |b| {
+            let mut cipher = crown::envelope::EvpStreamCipher::new_des_ctr(&key, &iv).unwrap();
+            let block = block.as_mut_slice();
+            b.iter(|| {
+                let _ = cipher.encrypt(block);
+            })
+        });
+
+        group.bench_function("boring".to_string(), |b| {
+            let cipher = boring::symm::Cipher::des_cbc();
+
+            b.iter(|| {
+                let _ = boring::symm::encrypt(cipher, &key, Some(&iv), &block);
+            });
+        });
+        group.finish();
+    }
+
+    for i in cases {
+        let mut block = vec![0u8; i];
+        rand::fill(block.as_mut_slice());
+
+        let mut group = c.benchmark_group(format!("des_gcm_{i}"));
+        group.throughput(Throughput::Bytes(i as u64));
+
+        group.bench_function("crown".to_string(), |b| {
+            let cipher = crown::envelope::EvpAeadCipher::new_des_gcm(&key).unwrap();
+            let block = &mut block;
+            b.iter(|| {
+                let _ = cipher.seal_in_place_separate_tag(block, &nonce, &[]);
+            })
+        });
+
+        group.finish();
+    }
 }
 
 criterion_group!(benches, bench_des);
