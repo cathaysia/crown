8dabc56cfc88af458fe772338831a3ce2983a83e
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-20 21:21:08 +0800

    refactor(derive): replace jinja with js
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crown-derive/src/lib.rs
+++ crown-derive/src/lib.rs
@@ -1,10 +1,7 @@
+mod js_engine;
 mod marshal;
 
-use minijinja::{context, Environment};
-use proc_macro::{Span, TokenStream};
-use quote::quote;
-use std::fs;
-use syn::{parse_macro_input, LitStr};
+use proc_macro::TokenStream;
 
 #[proc_macro_derive(Marshal, attributes(marshal))]
 pub fn marshal(input: TokenStream) -> TokenStream {
@@ -12,53 +9,11 @@ pub fn marshal(input: TokenStream) -> TokenStream {
 }
 
 #[proc_macro]
-pub fn jinja(input: TokenStream) -> TokenStream {
-    let tmpl = parse_macro_input!(input as LitStr);
-
-    let rendered = match render(&tmpl.value()) {
-        Ok(result) => result,
-        Err(err) => {
-            return syn::Error::new(
-                Span::call_site().into(),
-                format!("Jinja rendering error: {}", err),
-            )
-            .to_compile_error()
-            .into();
-        }
-    };
-
-    let expr = LitStr::new(&rendered, Span::call_site().into());
-    quote! { #expr }.into()
+pub fn jsasm(input: TokenStream) -> TokenStream {
+    js_engine::jsasm(input)
 }
 
 #[proc_macro]
-pub fn jinja_file(input: TokenStream) -> TokenStream {
-    let path = parse_macro_input!(input as LitStr).value();
-
-    let content = match fs::read_to_string(&path) {
-        Ok(content) => content,
-        Err(err) => {
-            return syn::Error::new(
-                Span::call_site().into(),
-                format!("Failed to read file '{}': {}", path, err),
-            )
-            .to_compile_error()
-            .into();
-        }
-    };
-
-    let content = LitStr::new(&content, Span::call_site().into());
-    jinja(
-        quote! {
-            #content
-        }
-        .into(),
-    )
-}
-
-fn render(template: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let env = Environment::new();
-    let tmpl = env.template_from_str(template)?;
-
-    Ok(tmpl.render(context! {})?)
+pub fn jsasm_file(input: TokenStream) -> TokenStream {
+    js_engine::jsasm_file(input)
 }
