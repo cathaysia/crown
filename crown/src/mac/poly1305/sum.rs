87f40a2fcf89f13a03894b7ee39dca9a2c1fb5d7
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-21 21:11:23 +0800

    refactor(asm): remove extra asm
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crown/src/mac/poly1305/sum.rs
+++ crown/src/mac/poly1305/sum.rs
@@ -1,16 +1,4 @@
 mod generic;
 pub use generic::*;
 
-#[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
-mod asm;
-#[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
-pub use asm::*;
-
-#[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
-pub type Mac = MacAarch64;
-
-#[cfg(any(
-    not(feature = "asm"),
-    not(any(target_arch = "aarch64", target_arch = "x86_64"))
-))]
 pub(crate) type Mac = MacGeneric;
--- crown/src/stream/chacha20/xor_key_stream/mod.rs
+++ crown/src/stream/chacha20/xor_key_stream/mod.rs
@@ -1,8 +1,1 @@
-#[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
-mod asm;
-
-#[cfg(any(
-    not(feature = "asm"),
-    not(any(target_arch = "aarch64", target_arch = "x86_64"))
-))]
 mod generic;
