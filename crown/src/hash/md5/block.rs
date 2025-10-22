f0189411b6a7d979c7172a40c85fbb7f31f42de3
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-22 20:17:16 +0800

    refactor(md5): remove aarch64 asm
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crown/src/hash/md5/block.rs
+++ crown/src/hash/md5/block.rs
@@ -1,20 +1,17 @@
 mod generic;
 
-#[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
+#[cfg(all(feature = "asm", target_arch = "x86_64"))]
 mod asm;
 
 use super::Md5;
 
 pub(super) fn block(d: &mut Md5, p: &[u8]) {
-    #[cfg(all(feature = "asm", any(target_arch = "aarch64", target_arch = "x86_64")))]
+    #[cfg(all(feature = "asm", target_arch = "x86_64"))]
     {
         asm::block(d, p);
     }
 
-    #[cfg(any(
-        not(feature = "asm"),
-        not(any(target_arch = "aarch64", target_arch = "x86_64"))
-    ))]
+    #[cfg(any(not(feature = "asm"), not(target_arch = "x86_64")))]
     {
         generic::block_generic(d, p);
     }
