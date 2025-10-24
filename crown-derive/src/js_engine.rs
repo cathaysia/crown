947e47ce7016968da53acae414919c76d8c6e108
loongtao.zhang <loongtao.zhang@outlook.com>
2025-10-24 05:22:55 +0000

    feat: allow import
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>
    
    feat: add jsasm
    
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crown-derive/src/js_engine.rs
+++ crown-derive/src/js_engine.rs
@@ -1,117 +1,40 @@
-use std::collections::HashMap;
+use std::path::Path;
 
+use anyhow::bail;
 use boa_engine::{Context, Source};
 use proc_macro::{Span, TokenStream};
 use quote::quote;
-use serde_json::Value;
-use syn::{parse::Parse, parse_macro_input, Error, Expr, LitStr, Token};
+use syn::{parse_macro_input, Error, LitStr};
 
-struct JinjaParam {
-    code: String,
-    context: HashMap<String, Value>,
-}
-
-impl Parse for JinjaParam {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let code: LitStr = input.parse()?;
-        let _: Token![,] = input.parse()?;
-        let context: LitStr = input.parse()?;
-        Ok(Self {
-            code: code.value(),
-            context: {
-                let mut map = HashMap::new();
-                let context_str = context.value();
-                if !context_str.is_empty() {
-                    if let Ok(parsed) = serde_json::from_str::<HashMap<String, Value>>(&context_str)
-                    {
-                        map = parsed
-                    }
-                }
-                map
-            },
-        })
-    }
-}
-
-struct JinjaFileParam {
-    path: String,
-    context: HashMap<String, Value>,
-}
-
-impl Parse for JinjaFileParam {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let path: LitStr = input.parse()?;
-        let mut context = HashMap::new();
-
-        while !input.is_empty() {
-            let _: Token![,] = input.parse()?;
-            if input.is_empty() {
-                break;
-            }
-
-            let key: LitStr = input.parse()?;
-            let _: Token![,] = input.parse()?;
-            let value: Expr = input.parse()?;
-
-            let key_str = key.value();
-            let value_json = expr_to_json_value(&value)?;
-            context.insert(key_str, value_json);
-        }
-
-        Ok(Self {
-            path: path.value(),
-            context,
-        })
-    }
-}
-
-fn expr_to_json_value(expr: &Expr) -> syn::Result<Value> {
-    match expr {
-        Expr::Lit(lit) => match &lit.lit {
-            syn::Lit::Str(s) => Ok(Value::String(s.value())),
-            syn::Lit::Int(i) => {
-                if let Ok(val) = i.base10_parse::<i64>() {
-                    Ok(Value::Number(serde_json::Number::from(val)))
-                } else {
-                    Err(Error::new_spanned(i, "Invalid integer"))
-                }
-            }
-            syn::Lit::Float(f) => {
-                if let Ok(val) = f.base10_parse::<f64>() {
-                    Ok(Value::Number(serde_json::Number::from_f64(val).unwrap()))
-                } else {
-                    Err(Error::new_spanned(f, "Invalid float"))
-                }
-            }
-            syn::Lit::Bool(b) => Ok(Value::Bool(b.value)),
-            _ => Err(Error::new_spanned(lit, "Unsupported literal type")),
-        },
-        _ => Err(Error::new_spanned(
-            expr,
-            "Only literals are supported as values",
-        )),
-    }
-}
-
-fn execute_js_with_json_context(
-    js_code: &str,
-    context: &HashMap<String, Value>,
-) -> Result<String, String> {
+fn execute_js_with_json_context(code: String, path: Option<String>) -> anyhow::Result<String> {
     let mut js_context = Context::default();
+    let code = match path {
+        Some(ref path) => std::fs::read_to_string(path)?,
+        None => code,
+    };
 
-    if !context.is_empty() {
-        let context_json = serde_json::to_string(context)
-            .map_err(|e| format!("Failed to serialize context: {}", e))?;
-        let context_js_code = format!("const CONTEXT = {};", context_json);
-        if let Err(e) = js_context.eval(Source::from_bytes(&context_js_code)) {
-            return Err(format!("Failed to evaluate context: {}", e));
+    {
+        let context = std::env::var("JSASM_VAR").unwrap();
+        let context = format!("globalThis.__CONTEXT = {:?};", context);
+        if let Err(e) = js_context.eval(Source::from_bytes(&context)) {
+            bail!("Failed to evaluate context: {}", e);
         }
     }
 
-    let result = match js_context.eval(Source::from_bytes(js_code)) {
+    let global = Source::from_bytes(include_bytes!("../jsasm/index.js"));
+    if let Err(err) = js_context.eval(global) {
+        bail!("Failed to evaluate global: {err}");
+    }
+
+    let mut source = Source::from_bytes(&code);
+    let path = path.as_ref().map(Path::new);
+    if let Some(path) = path {
+        source = source.with_path(path);
+    }
+    let result = match js_context.eval(source) {
         Ok(result) => result,
         Err(e) => {
-            return Err(format!("Failed to evaluate JavaScript code: {}", e));
+            bail!("Failed to evaluate JavaScript code: {}", e);
         }
     };
     let result = result.display().to_string();
@@ -121,9 +44,8 @@ fn execute_js_with_json_context(
 }
 
 pub fn jsasm(input: TokenStream) -> TokenStream {
-    let JinjaParam { code, context } = parse_macro_input!(input as JinjaParam);
-
-    match execute_js_with_json_context(&code, &context) {
+    let code = parse_macro_input!(input as LitStr).value();
+    match execute_js_with_json_context(code, None) {
         Ok(result) => {
             let expr = LitStr::new(&result, Span::call_site().into());
             quote! { #expr }.into()
@@ -135,21 +57,9 @@ pub fn jsasm(input: TokenStream) -> TokenStream {
 }
 
 pub fn jsasm_file(input: TokenStream) -> TokenStream {
-    let JinjaFileParam { path, context } = parse_macro_input!(input as JinjaFileParam);
+    let path = parse_macro_input!(input as LitStr).value();
 
-    let code_content = match std::fs::read_to_string(&path) {
-        Ok(content) => content,
-        Err(e) => {
-            return Error::new(
-                Span::call_site().into(),
-                format!("Failed to read file: {}", e),
-            )
-            .to_compile_error()
-            .into();
-        }
-    };
-
-    match execute_js_with_json_context(&code_content, &context) {
+    match execute_js_with_json_context(String::default(), Some(path)) {
         Ok(result) => {
             let expr = LitStr::new(&result, Span::call_site().into());
             quote! { #expr }.into()
