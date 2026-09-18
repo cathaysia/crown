a3350b26d8d213fae35adaefdf1106613f1c7c88
loongtao.zhang <loongtao.zhang@outlook.com>
2026-09-19 01:41:33 +0800

    feat(asm): wire cpuid asm and OPENSSL_ia32cap_P initialization
    rewrite the cpuid generator as a 1:1 port of crypto/x86_64cpuid.pl,
    byte-identical to 'perl x86_64cpuid.pl elf' (modulo the llvm-friendly
    .note.gnu.property section form), and enable it behind the asm feature.
    OPENSSL_cpuid_setup is implemented in rust and runs from the .init stub
    the assembly emits, filling OPENSSL_ia32cap_P so the asm dispatchers
    see real cpu feature flags.
    
    also fix three xlate issues found while wiring: pin the gnuas flavour
    (rustc consumes the generated asm with llvm, which needs the quoted
    .section flag form), mirror perl oct() for 0x-prefixed pclmulqdq/vprot
    immediates, and stop an explicitly-empty opcode size (movd with an xmm
    operand) from being overwritten by register size inference.

--- crown/src/utils/cpuid/x86_64.ts
+++ crown/src/utils/cpuid/x86_64.ts
@@ -1,293 +1,253 @@
-function generateAssembly(output?: string, flavour?: string): string {
-  let win64 = 0;
-  if (
-    flavour &&
-    (/[nm]asm|mingw64/.test(flavour) || (output && /\.asm$/.test(output)))
-  ) {
-    win64 = 1;
-  }
+/**
+ * x86_64 BIGNUM converting upload, CPUID and RDTSC handling.
+ *
+ * TypeScript port of OpenSSL crypto/x86_64cpuid.pl.
+ * Copyright 2005-2026 The OpenSSL Project Authors. All Rights Reserved.
+ * Licensed under Apache License 2.0.
+ *
+ * Pinned to the reference configuration: $win64=0 (unix argument
+ * registers), $avx=0.
+ */
 
-  let arg1: string, arg2: string, arg3: string, arg4: string;
-  if (win64) {
-    [arg1, arg2, arg3, arg4] = ['%rcx', '%rdx', '%r8', '%r9'];
-  } else {
-    [arg1, arg2, arg3, arg4] = ['%rdi', '%rsi', '%rdx', '%rcx'];
-  }
+import { translateAssembly } from 'jsasm/x86_64-xlate';
 
-  function genRandom(rdop: string): string {
-    return `
-.globl	OPENSSL_ia32_${rdop}_bytes
-.type	OPENSSL_ia32_${rdop}_bytes,\\@abi-omnipotent
-.align	16
-OPENSSL_ia32_${rdop}_bytes:
-.cfi_startproc
-	endbranch
-	xor	%rax, %rax
-	cmp	\\$0,${arg2}
-	je	.Ldone_${rdop}_bytes
+let code = '';
 
-	mov	\\$8,%r11
-.Loop_${rdop}_bytes:
-	${rdop}	%r10
-	jc	.Lbreak_${rdop}_bytes
-	dec	%r11
-	jnz	.Loop_${rdop}_bytes
-	jmp	.Ldone_${rdop}_bytes
+const arg1 = '%rdi';
+const arg2 = '%rsi';
+const arg3 = '%rdx';
+const arg4 = '%rcx';
 
-.align	16
-.Lbreak_${rdop}_bytes:
-	cmp	\\$8,${arg2}
-	jb	.Ltail_${rdop}_bytes
-	mov	%r10,(${arg1})
-	lea	8(${arg1}),${arg1}
-	add	\\$8,%rax
-	sub	\\$8,${arg2}
-	jz	.Ldone_${rdop}_bytes
-	mov	\\$8,%r11
-	jmp	.Loop_${rdop}_bytes
-
-.align	16
-.Ltail_${rdop}_bytes:
-	mov	%r10b,(${arg1})
-	lea	1(${arg1}),${arg1}
-	inc	%rax
-	shr	\\$8,%r10
-	dec	${arg2}
-	jnz	.Ltail_${rdop}_bytes
-
-.Ldone_${rdop}_bytes:
-	xor	%r10,%r10
-	ret
-.cfi_endproc
-.size	OPENSSL_ia32_${rdop}_bytes,.-OPENSSL_ia32_${rdop}_bytes`;
-  }
-
-  const out = '%r10';
-  const cnt = '%rcx';
-  const max = '%r11';
-  const lasttick = '%r8d';
-  const lastdiff = '%r9d';
-  const redzone = win64 ? 8 : -8;
-
-  return `#include crypto/cryptlib.h
+code += `#include crypto/cryptlib.h
 .extern		OPENSSL_cpuid_setup
 .hidden		OPENSSL_cpuid_setup
+.section	.init
+	call	OPENSSL_cpuid_setup
+
 .hidden	OPENSSL_ia32cap_P
-.comm	OPENSSL_ia32cap_P,40,4
+.comm	OPENSSL_ia32cap_P,40,4	# <--Should match with internal/cryptlib.h OPENSSL_IA32CAP_P_MAX_INDEXES
 .text
 
 .globl	OPENSSL_atomic_add
-.type	OPENSSL_atomic_add,\\@abi-omnipotent
+.type	OPENSSL_atomic_add,@abi-omnipotent
 .align	16
 OPENSSL_atomic_add:
 .cfi_startproc
 	endbranch
 	movl	(${arg1}),%eax
 .Lspin:	leaq	(${arg2},%rax),%r8
-	.byte	0xf0
+	.byte	0xf0		# lock
 	cmpxchgl	%r8d,(${arg1})
 	jne	.Lspin
 	movl	%r8d,%eax
-	.byte	0x48,0x98
+	.byte	0x48,0x98	# cltq/cdqe
 	ret
 .cfi_endproc
 .size	OPENSSL_atomic_add,.-OPENSSL_atomic_add
 
 .globl	OPENSSL_rdtsc
-.type	OPENSSL_rdtsc,\\@abi-omnipotent
+.type	OPENSSL_rdtsc,@abi-omnipotent
 .align	16
 OPENSSL_rdtsc:
 .cfi_startproc
 	endbranch
 	rdtsc
-	shl	\\$32,%rdx
+	shl	$32,%rdx
 	or	%rdx,%rax
 	ret
 .cfi_endproc
 .size	OPENSSL_rdtsc,.-OPENSSL_rdtsc
 
 .globl	OPENSSL_ia32_cpuid
-.type	OPENSSL_ia32_cpuid,\\@function,1
+.type	OPENSSL_ia32_cpuid,@function,1
 .align	16
 OPENSSL_ia32_cpuid:
 .cfi_startproc
 	endbranch
-	mov	%rbx,%r8
+	mov	%rbx,%r8		# save %rbx
 .cfi_register	%rbx,%r8
 
 	xor	%eax,%eax
-	mov	%rax,8(%rdi)
+	mov	%rax,8(%rdi)		# clear extended feature flags
 	cpuid
-	mov	%eax,%r11d
+	mov	%eax,%r11d		# max value for standard query level
 
 	xor	%eax,%eax
-	cmp	\\$0x756e6547,%ebx
+	cmp	$0x756e6547,%ebx	# "Genu"
 	setne	%al
 	mov	%eax,%r9d
-	cmp	\\$0x49656e69,%edx
+	cmp	$0x49656e69,%edx	# "ineI"
 	setne	%al
 	or	%eax,%r9d
-	cmp	\\$0x6c65746e,%ecx
+	cmp	$0x6c65746e,%ecx	# "ntel"
 	setne	%al
-	or	%eax,%r9d
+	or	%eax,%r9d		# 0 indicates Intel CPU
 	jz	.Lintel
 
-	cmp	\\$0x68747541,%ebx
+	cmp	$0x68747541,%ebx	# "Auth"
 	setne	%al
 	mov	%eax,%r10d
-	cmp	\\$0x69746E65,%edx
+	cmp	$0x69746E65,%edx	# "enti"
 	setne	%al
 	or	%eax,%r10d
-	cmp	\\$0x444D4163,%ecx
+	cmp	$0x444D4163,%ecx	# "cAMD"
 	setne	%al
-	or	%eax,%r10d
+	or	%eax,%r10d		# 0 indicates AMD CPU
 	jnz	.Lintel
 
-	mov	\\$0x80000000,%eax
+	# AMD specific
+	mov	$0x80000000,%eax
 	cpuid
-	cmp	\\$0x80000001,%eax
+	cmp	$0x80000001,%eax
 	jb	.Lintel
 	mov	%eax,%r10d
-	mov	\\$0x80000001,%eax
+	mov	$0x80000001,%eax
 	cpuid
 	or	%ecx,%r9d
-	and	\\$0x00000801,%r9d
+	and	$0x00000801,%r9d	# isolate AMD XOP bit, 1<<11
 
-	cmp	\\$0x80000008,%r10d
+	cmp	$0x80000008,%r10d
 	jb	.Lintel
 
-	mov	\\$0x80000008,%eax
+	mov	$0x80000008,%eax
 	cpuid
-	movzb	%cl,%r10
-	inc	%r10
+	movzb	%cl,%r10		# number of cores - 1
+	inc	%r10			# number of cores
 
-	mov	\\$1,%eax
+	mov	$1,%eax
 	cpuid
-	bt	\\$28,%edx
+	bt	$28,%edx		# test hyper-threading bit
 	jnc	.Lgeneric
-	shr	\\$16,%ebx
+	shr	$16,%ebx		# number of logical processors
 	cmp	%r10b,%bl
 	ja	.Lgeneric
-	and	\\$0xefffffff,%edx
+	and	$0xefffffff,%edx	# ~(1<<28)
 	jmp	.Lgeneric
 
 .Lintel:
-	cmp	\\$4,%r11d
-	mov	\\$-1,%r10d
+	cmp	$4,%r11d
+	mov	$-1,%r10d
 	jb	.Lnocacheinfo
 
-	mov	\\$4,%eax
-	mov	\\$0,%ecx
+	mov	$4,%eax
+	mov	$0,%ecx		# query L1D
 	cpuid
 	mov	%eax,%r10d
-	shr	\\$14,%r10d
-	and	\\$0xfff,%r10d
+	shr	$14,%r10d
+	and	$0xfff,%r10d		# number of cores -1 per L1D
 
 .Lnocacheinfo:
-	mov	\\$1,%eax
+	mov	$1,%eax
 	cpuid
-	movd	%eax,%xmm0
-	and	\\$0xbfefffff,%edx
-	cmp	\\$0,%r9d
+	movd	%eax,%xmm0		# put aside processor id
+	and	$0xbfefffff,%edx	# force reserved bits to 0
+	cmp	$0,%r9d
 	jne	.Lnotintel
-	or	\\$0x40000000,%edx
-	and	\\$15,%ah
-	cmp	\\$15,%ah
+	or	$0x40000000,%edx	# set reserved bit#30 on Intel CPUs
+	and	$15,%ah
+	cmp	$15,%ah		# examine Family ID
 	jne	.LnotP4
-	or	\\$0x00100000,%edx
+	or	$0x00100000,%edx	# set reserved bit#20 to engage RC4_CHAR
 .LnotP4:
-	cmp	\\$6,%ah
+	cmp	$6,%ah
 	jne	.Lnotintel
-	and	\\$0x0fff0ff0,%eax
-	cmp	\\$0x00050670,%eax
+	and	$0x0fff0ff0,%eax
+	cmp	$0x00050670,%eax	# Knights Landing
 	je	.Lknights
-	cmp	\\$0x00080650,%eax
+	cmp	$0x00080650,%eax	# Knights Mill (according to sde)
 	jne	.Lnotintel
 .Lknights:
-	and	\\$0xfbffffff,%ecx
+	and	$0xfbffffff,%ecx	# clear XSAVE flag to mimic Silvermont
 
 .Lnotintel:
-	bt	\\$28,%edx
+	bt	$28,%edx		# test hyper-threading bit
 	jnc	.Lgeneric
-	and	\\$0xefffffff,%edx
-	cmp	\\$0,%r10d
+	and	$0xefffffff,%edx	# ~(1<<28)
+	cmp	$0,%r10d
 	je	.Lgeneric
 
-	or	\\$0x10000000,%edx
-	shr	\\$16,%ebx
-	cmp	\\$1,%bl
+	or	$0x10000000,%edx	# 1<<28
+	shr	$16,%ebx
+	cmp	$1,%bl			# see if cache is shared
 	ja	.Lgeneric
-	and	\\$0xefffffff,%edx
+	and	$0xefffffff,%edx	# ~(1<<28)
 .Lgeneric:
-	and	\\$0x00000800,%r9d
-	and	\\$0xfffff7ff,%ecx
-	or	%ecx,%r9d
+	and	$0x00000800,%r9d	# isolate AMD XOP flag
+	and	$0xfffff7ff,%ecx
+	or	%ecx,%r9d		# merge AMD XOP flag
 
-	mov	%edx,%r10d
+	mov	%edx,%r10d		# %r9d:%r10d is copy of %ecx:%edx
 
-	cmp	\\$7,%r11d
+	cmp	$7,%r11d
 	jb	.Lno_extended_info
-	mov	\\$7,%eax
+	mov	$7,%eax
 	xor	%ecx,%ecx
 	cpuid
-	movd	%eax,%xmm1
-	bt	\\$26,%r9d
+	movd	%eax,%xmm1		# put aside leaf 07H Max Sub-leaves
+	bt	$26,%r9d		# check XSAVE bit, cleared on Knights
 	jc	.Lnotknights
-	and	\\$0xfff7ffff,%ebx
+	and	$0xfff7ffff,%ebx	# clear ADCX/ADOX flag
 .Lnotknights:
-	movd	%xmm0,%eax
-	and	\\$0x0fff0ff0,%eax
-	cmp	\\$0x00050650,%eax
+	movd	%xmm0,%eax		# restore processor id
+	and	$0x0fff0ff0,%eax
+	cmp	$0x00050650,%eax	# Skylake-X
 	jne	.Lnotskylakex
-	and	\\$0xfffeffff,%ebx
+	and	$0xfffeffff,%ebx	# ~(1<<16)
+					# suppress AVX512F flag on Skylake-X
 
-.Lnotskylakex:
-	mov	%ebx,8(%rdi)
-	mov	%ecx,12(%rdi)
-	mov	%edx,16(%rdi)
+.Lnotskylakex:		# save extended feature flags
+	mov	%ebx,8(%rdi)		# save cpuid(EAX=0x7, ECX=0x0).EBX to OPENSSL_ia32cap_P[2]
+	mov	%ecx,12(%rdi)		# save cpuid(EAX=0x7, ECX=0x0).ECX to OPENSSL_ia32cap_P[3]
+	mov	%edx,16(%rdi)		# save cpuid(EAX=0x7, ECX=0x0).EDX to OPENSSL_ia32cap_P[4]
 
-	movd	%xmm1,%eax
-	cmp	\\$0x1,%eax
+	movd	%xmm1,%eax		# Restore leaf 07H Max Sub-leaves
+	cmp	$0x1,%eax		# Do we have cpuid(EAX=0x7, ECX=0x1)?
 	jb .Lno_extended_info
-	mov	\\$0x7,%eax
-	mov \\$0x1,%ecx
-	cpuid
-	mov	%eax,20(%rdi)
-	mov	%edx,24(%rdi)
-	mov	%ebx,28(%rdi)
-	mov	%ecx,32(%rdi)
+	mov	$0x7,%eax
+	mov $0x1,%ecx
+	cpuid		# cpuid(EAX=0x7, ECX=0x1)
+	mov	%eax,20(%rdi)		# save cpuid(EAX=0x7, ECX=0x1).EAX to OPENSSL_ia32cap_P[5]
+	mov	%edx,24(%rdi)		# save cpuid(EAX=0x7, ECX=0x1).EDX to OPENSSL_ia32cap_P[6]
+	mov	%ebx,28(%rdi)		# save cpuid(EAX=0x7, ECX=0x1).EBX to OPENSSL_ia32cap_P[7]
+	mov	%ecx,32(%rdi)		# save cpuid(EAX=0x7, ECX=0x1).ECX to OPENSSL_ia32cap_P[8]
 
-	and \\$0x80000,%edx
-	cmp \\$0x0,%edx
+	and $0x80000,%edx		# Mask cpuid(EAX=0x7, ECX=0x1).EDX bit 19 to detect AVX10 support
+	cmp $0x0,%edx
 	je .Lno_extended_info
-	mov	\\$0x24,%eax
-	mov \\$0x0,%ecx
-	cpuid
-	mov	%ebx,36(%rdi)
+	mov	$0x24,%eax		# Have AVX10 Support, query for details
+	mov $0x0,%ecx
+	cpuid		# cpuid(EAX=0x24, ECX=0x0) AVX10 Leaf
+	mov	%ebx,36(%rdi)		# save cpuid(EAX=0x24, ECX=0x0).EBX to OPENSSL_ia32cap_P[9]
 
 .Lno_extended_info:
 
-	bt	\\$27,%r9d
+	bt	$27,%r9d		# check OSXSAVE bit
 	jnc	.Lclear_avx
-	xor	%ecx,%ecx
-	.byte	0x0f,0x01,0xd0
-	and	\\$0xe6,%eax
-	cmp	\\$0xe6,%eax
+	xor	%ecx,%ecx		# XCR0
+	.byte	0x0f,0x01,0xd0		# xgetbv
+	and	$0xe6,%eax		# isolate XMM, YMM and ZMM state support
+	cmp	$0xe6,%eax
 	je	.Ldone
-	andl	\\$0x3fdeffff,8(%rdi)
-	and	\\$6,%eax
-	cmp	\\$6,%eax
+	andl	$0x3fdeffff,8(%rdi)	# ~(1<<31|1<<30|1<<21|1<<16)
+					# clear AVX512F+BW+VL+IFMA, all of
+					# them are EVEX-encoded, which requires
+					# ZMM state support even if one uses
+					# only XMM and YMM :-(
+	and	$6,%eax		# isolate XMM and YMM state support
+	cmp	$6,%eax
 	je	.Ldone
 .Lclear_avx:
-	andl	\\$0xff7fffff,20(%rdi)
-	mov	\\$0xefffe7ff,%eax
-	and	%eax,%r9d
-	mov	\\$0x3fdeffdf,%eax
-	and	%eax,8(%rdi)
+	andl	$0xff7fffff,20(%rdi)   # ~(1<<23)
+					# clear AVXIFMA, which is VEX-encoded
+					# and requires YMM state support
+	mov	$0xefffe7ff,%eax	# ~(1<<28|1<<12|1<<11)
+	and	%eax,%r9d		# clear AVX, FMA and AMD XOP bits
+	mov	$0x3fdeffdf,%eax	# ~(1<<31|1<<30|1<<21|1<<16|1<<5)
+	and	%eax,8(%rdi)		# clear AVX2 and AVX512* bits
 .Ldone:
-	shl	\\$32,%r9
+	shl	$32,%r9
 	mov	%r10d,%eax
-	mov	%r8,%rbx
+	mov	%r8,%rbx		# restore %rbx
 .cfi_restore	%rbx
 	or	%r9,%rax
 	ret
@@ -295,26 +255,26 @@ OPENSSL_ia32_cpuid:
 .size	OPENSSL_ia32_cpuid,.-OPENSSL_ia32_cpuid
 
 .globl  OPENSSL_cleanse
-.type   OPENSSL_cleanse,\\@abi-omnipotent
+.type   OPENSSL_cleanse,@abi-omnipotent
 .align  16
 OPENSSL_cleanse:
 .cfi_startproc
 	endbranch
 	xor	%rax,%rax
-	cmp	\\$15,${arg2}
+	cmp	$15,${arg2}
 	jae	.Lot
-	cmp	\\$0,${arg2}
+	cmp	$0,${arg2}
 	je	.Lret
 .Little:
 	mov	%al,(${arg1})
-	sub	\\$1,${arg2}
+	sub	$1,${arg2}
 	lea	1(${arg1}),${arg1}
 	jnz	.Little
 .Lret:
 	ret
 .align	16
 .Lot:
-	test	\\$7,${arg1}
+	test	$7,${arg1}
 	jz	.Laligned
 	mov	%al,(${arg1})
 	lea	-1(${arg2}),${arg2}
@@ -323,30 +283,30 @@ OPENSSL_cleanse:
 .Laligned:
 	mov	%rax,(${arg1})
 	lea	-8(${arg2}),${arg2}
-	test	\\$-8,${arg2}
+	test	$-8,${arg2}
 	lea	8(${arg1}),${arg1}
 	jnz	.Laligned
-	cmp	\\$0,${arg2}
+	cmp	$0,${arg2}
 	jne	.Little
 	ret
 .cfi_endproc
 .size	OPENSSL_cleanse,.-OPENSSL_cleanse
 
 .globl  CRYPTO_memcmp
-.type   CRYPTO_memcmp,\\@abi-omnipotent
+.type   CRYPTO_memcmp,@abi-omnipotent
 .align  16
 CRYPTO_memcmp:
 .cfi_startproc
 	endbranch
 	xor	%rax,%rax
 	xor	%r10,%r10
-	cmp	\\$0,${arg3}
+	cmp	$0,${arg3}
 	je	.Lno_data
-	cmp	\\$16,${arg3}
+	cmp	$16,${arg3}
 	jne	.Loop_cmp
 	mov	(${arg1}),%r10
 	mov	8(${arg1}),%r11
-	mov	\\$1,${arg3}
+	mov	$1,${arg3}
 	xor	(${arg2}),%r10
 	xor	8(${arg2}),%r11
 	or	%r11,%r10
@@ -363,27 +323,38 @@ CRYPTO_memcmp:
 	dec	${arg3}
 	jnz	.Loop_cmp
 	neg	%rax
-	shr	\\$63,%rax
+	shr	$63,%rax
 .Lno_data:
 	ret
 .cfi_endproc
 .size	CRYPTO_memcmp,.-CRYPTO_memcmp
+`;
 
-.globl	OPENSSL_instrument_bus
-.type	OPENSSL_instrument_bus,\\@abi-omnipotent
+{
+  const out = '%r10';
+  const cnt = '%rcx';
+  const max = '%r11';
+  const lasttick = '%r8d';
+  const lastdiff = '%r9d';
+  // The perl reads `win64?8:-8` where win64 is a bareword (no use strict),
+  // which evaluates as the truthy string 'win64', so this is always 8.
+  const redzone = 8;
+
+  code += `.globl	OPENSSL_instrument_bus
+.type	OPENSSL_instrument_bus,@abi-omnipotent
 .align	16
 OPENSSL_instrument_bus:
 .cfi_startproc
 	endbranch
-	mov	${arg1},${out}
+	mov	${arg1},${out}	# tribute to Win64
 	mov	${arg2},${cnt}
 	mov	${arg2},${max}
 
-	rdtsc
-	mov	%eax,${lasttick}
-	mov	\\$0,${lastdiff}
+	rdtsc			# collect 1st tick
+	mov	%eax,${lasttick}	# lasttick = tick
+	mov	$0,${lastdiff}	# lastdiff = 0
 	clflush	(${out})
-	.byte	0xf0
+	.byte	0xf0		# lock
 	add	${lastdiff},(${out})
 	jmp	.Loop
 .align	16
@@ -393,10 +364,10 @@ OPENSSL_instrument_bus:
 	mov	%edx,${lasttick}
 	mov	%eax,${lastdiff}
 	clflush	(${out})
-	.byte	0xf0
+	.byte	0xf0		# lock
 	add	%eax,(${out})
 	lea	4(${out}),${out}
-	sub	\\$1,${cnt}
+	sub	$1,${cnt}
 	jnz	.Loop
 
 	mov	${max},%rax
@@ -405,47 +376,47 @@ OPENSSL_instrument_bus:
 .size	OPENSSL_instrument_bus,.-OPENSSL_instrument_bus
 
 .globl	OPENSSL_instrument_bus2
-.type	OPENSSL_instrument_bus2,\\@abi-omnipotent
+.type	OPENSSL_instrument_bus2,@abi-omnipotent
 .align	16
 OPENSSL_instrument_bus2:
 .cfi_startproc
 	endbranch
-	mov	${arg1},${out}
+	mov	${arg1},${out}	# tribute to Win64
 	mov	${arg2},${cnt}
 	mov	${arg3},${max}
 	mov	${cnt},${redzone}(%rsp)
 
-	rdtsc
-	mov	%eax,${lasttick}
-	mov	\\$0,${lastdiff}
+	rdtsc			# collect 1st tick
+	mov	%eax,${lasttick}	# lasttick = tick
+	mov	$0,${lastdiff}	# lastdiff = 0
 
 	clflush	(${out})
-	.byte	0xf0
+	.byte	0xf0		# lock
 	add	${lastdiff},(${out})
 
-	rdtsc
+	rdtsc			# collect 1st diff
 	mov	%eax,%edx
-	sub	${lasttick},%eax
-	mov	%edx,${lasttick}
-	mov	%eax,${lastdiff}
+	sub	${lasttick},%eax	# diff
+	mov	%edx,${lasttick}	# lasttick = tick
+	mov	%eax,${lastdiff}	# lastdiff = diff
 .Loop2:
 	clflush	(${out})
-	.byte	0xf0
-	add	%eax,(${out})
+	.byte	0xf0		# lock
+	add	%eax,(${out})	# accumulate diff
 
-	sub	\\$1,${max}
+	sub	$1,${max}
 	jz	.Ldone2
 
 	rdtsc
 	mov	%eax,%edx
-	sub	${lasttick},%eax
-	mov	%edx,${lasttick}
+	sub	${lasttick},%eax	# diff
+	mov	%edx,${lasttick}	# lasttick = tick
 	cmp	${lastdiff},%eax
-	mov	%eax,${lastdiff}
-	mov	\\$0,%edx
+	mov	%eax,${lastdiff}	# lastdiff = diff
+	mov	$0,%edx
 	setne	%dl
-	sub	%rdx,${cnt}
-	lea	(${out},%rdx,4),${out}
+	sub	%rdx,${cnt}	# conditional --${cnt}
+	lea	(${out},%rdx,4),${out}	# conditional ++${out}
 	jnz	.Loop2
 
 .Ldone2:
@@ -454,10 +425,58 @@ OPENSSL_instrument_bus2:
 	ret
 .cfi_endproc
 .size	OPENSSL_instrument_bus2,.-OPENSSL_instrument_bus2
-
-${genRandom('rdrand')}
-
-${genRandom('rdseed')}
 `;
 }
-export default generateAssembly();
+
+function genRandom(rdop: string): void {
+  code += `.globl	OPENSSL_ia32_${rdop}_bytes
+.type	OPENSSL_ia32_${rdop}_bytes,@abi-omnipotent
+.align	16
+OPENSSL_ia32_${rdop}_bytes:
+.cfi_startproc
+	endbranch
+	xor	%rax, %rax	# return value
+	cmp	$0,${arg2}
+	je	.Ldone_${rdop}_bytes
+
+	mov	$8,%r11
+.Loop_${rdop}_bytes:
+	${rdop}	%r10
+	jc	.Lbreak_${rdop}_bytes
+	dec	%r11
+	jnz	.Loop_${rdop}_bytes
+	jmp	.Ldone_${rdop}_bytes
+
+.align	16
+.Lbreak_${rdop}_bytes:
+	cmp	$8,${arg2}
+	jb	.Ltail_${rdop}_bytes
+	mov	%r10,(${arg1})
+	lea	8(${arg1}),${arg1}
+	add	$8,%rax
+	sub	$8,${arg2}
+	jz	.Ldone_${rdop}_bytes
+	mov	$8,%r11
+	jmp	.Loop_${rdop}_bytes
+
+.align	16
+.Ltail_${rdop}_bytes:
+	mov	%r10b,(${arg1})
+	lea	1(${arg1}),${arg1}
+	inc	%rax
+	shr	$8,%r10
+	dec	${arg2}
+	jnz	.Ltail_${rdop}_bytes
+
+.Ldone_${rdop}_bytes:
+	xor	%r10,%r10	# Clear sensitive data from register
+	ret
+.cfi_endproc
+.size	OPENSSL_ia32_${rdop}_bytes,.-OPENSSL_ia32_${rdop}_bytes
+`;
+}
+
+genRandom('rdrand');
+genRandom('rdseed');
+
+export default translateAssembly(code);
