56674bd4659684a02967a9e81b9e5e50feb09d1b
loongtao.zhang <loongtao.zhang@outlook.com>
2026-09-19 01:46:22 +0800

    feat(asm): add keccak1600 x86_64 asm
    port crypto/sha/asm/keccak1600-x86_64.pl to typescript, byte-identical
    to 'perl keccak1600-x86_64.pl elf' including the perl list-assignment
    register permutations between heredocs. wire KeccakF1600 into the sha3
    dispatch behind the asm feature.

--- crown/src/hash/sha3/noasm.rs
+++ crown/src/hash/sha3/noasm.rs
@@ -1,3 +1,11 @@
 pub fn keccak_f1600(da: &mut [u8; 200]) {
-    super::keccakf::keccak_f1600_generic(da);
+    #[cfg(all(feature = "asm", target_arch = "x86_64"))]
+    {
+        super::asm::keccak_f1600(da);
+    }
+
+    #[cfg(any(not(feature = "asm"), not(target_arch = "x86_64")))]
+    {
+        super::keccakf::keccak_f1600_generic(da);
+    }
 }
